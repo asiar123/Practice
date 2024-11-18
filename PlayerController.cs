8c25210f4243ac44c383d6dff6ebd0850d6348da
asiar123 <michael_2504@hotmail.com>
2024-11-18 10:11:38 -0500

    first commit

---
+++ PlayerController.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform rifleStart;
+    [SerializeField] private Text HpText;
+
+    [SerializeField] private GameObject GameOver;
+    [SerializeField] private GameObject Victory;
+
+    public float health = 100;
+    public float movementSpeed = 5f;
+
+    private float ySpeed = 0f;
+    private Vector3 moveDirection;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void ChangeHealth(int hp)
+    {
+        health += hp;
+        if (health > 100)
+        {
+            health = 100;
+        }
+        else if (health <= 0)
+        {
+            Lost();
+        }
+        HpText.text = health.ToString();
+    }
+
+    public void Win()
+    {
+        Victory.SetActive(true);
+        Destroy(GetComponent<PlayerLook>());
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Lost()
+    {
+        GameOver.SetActive(true);
+        Destroy(GetComponent<PlayerLook>());
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        //Crear movimiento
+        moveDirection = new Vector3(moveX,ySpeed,moveZ);
+        //Conversion de las coordendas geograficas
+        moveDirection = transform.TransformDirection(moveDirection);
+        //Asignar la velocidad
+        moveDirection *= movementSpeed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject buf = Instantiate(bullet);
+            buf.transform.position = rifleStart.position;
+            buf.GetComponent<Bullet>().setDirection(transform.forward);
+            buf.transform.rotation = transform.rotation;
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Collider[] tar = Physics.OverlapSphere(transform.position, 2);
+            foreach (var item in tar)
+            {
+                if (item.tag == "Enemy")
+                {
+                    Destroy(item.gameObject);
+                }
+            }
+        }
+
+        Collider[] targets = Physics.OverlapSphere(transform.position, 3);
+        foreach (var item in targets)
+        {
+            if (item.tag == "Heal")
+            {
+                ChangeHealth(50);
+                Destroy(item.gameObject);
+            }
+            if (item.tag == "Finish")
+            {
+                Win();
+            }
+            if (item.tag == "Enemy")
+            {
+                Lost();
+            }
+        }
+    }
+}
